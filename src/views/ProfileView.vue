6944c4ddcd23aa0eba2dbe1e5f5beb6e5cfd58af
Javier Rivera <javierivera29@gmail.com>
2024-11-30 06:27:57 -0600

    Datos de DB a normal

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,49 +1,60 @@
 <template>
-    <div class="profile-view-container p-4">
-      <!-- Formulario de perfil -->
-      <ProfileForm />
-  
-      <!-- Agregar el componente CardList debajo del formulario -->
-      <CardList @open-modal="openModal" />
-      
-      <!-- Modal para agregar/actualizar tarjeta -->
-      <AddUpdateCardModal 
-        :isOpen="isModalOpen" 
-        :card="selectedCard" 
-        @close="closeModal" 
-      />
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  
-  // Importar el componente ProfileForm, CardList y AddUpdateModal
-  import ProfileForm from "@/components/ProfileForm.vue";
-  import CardList from "@/components/CardList.vue";
-  import AddUpdateCardModal from "@/components/AddUpdateCardModal.vue";
-  
-  // Estado para controlar la visibilidad del modal y la tarjeta seleccionada
-  const isModalOpen = ref(false);
-  const selectedCard = ref(null);
-  
-  // Función para abrir el modal con la tarjeta seleccionada o agregar una nueva
-  const openModal = (card = null) => {
-    selectedCard.value = card;
-    isModalOpen.value = true;
-  };
-  
-  // Función para cerrar el modal
-  const closeModal = () => {
-    isModalOpen.value = false;
-    selectedCard.value = null;
-  };
-  </script>
-  
-  <style scoped>
-  .profile-view-container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
+  <div class="profile-view-container p-4">
+    <!-- Formulario de perfil -->
+    <ProfileForm />
+
+    <!-- Agregar el componente CardList debajo del formulario -->
+    <CardList @open-modal="openModal" />
+
+    <!-- Modal para agregar/actualizar tarjeta -->
+    <AddUpdateCardModal
+      :isOpen="isModalOpen"
+      :card="selectedCard"
+      @close="closeModal"
+      @update:card="handleUpdateCard"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// Importar el componente ProfileForm, CardList y AddUpdateModal
+import ProfileForm from '@/components/ProfileForm.vue'
+import CardList from '@/components/CardList.vue'
+import AddUpdateCardModal from '@/components/AddUpdateCardModal.vue'
+
+// Estado para controlar la visibilidad del modal y la tarjeta seleccionada
+const isModalOpen = ref(false)
+const selectedCard = ref<Card>()
+
+// Función para abrir el modal con la tarjeta seleccionada o agregar una nueva
+const openModal = (card?: Card) => {
+  if (card) selectedCard.value = card
+  isModalOpen.value = true
+}
+
+// Función para cerrar el modal
+const closeModal = () => {
+  isModalOpen.value = false
+  selectedCard.value = {
+    id: '',
+    number: '',
+    name: '',
+    expDate: '',
+    cvv: ''
   }
-  </style>
+}
+
+const handleUpdateCard = (newValue: Card) => {
+  selectedCard.value = newValue
+}
+</script>
+
+<style scoped>
+.profile-view-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
